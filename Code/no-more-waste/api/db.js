01e9820c4dab510349e29a796e234974ac76ff1c
bulbularora <bulbularora123@gmail.com>
2022-11-14 20:40:19 -0600

    commit connection

--- Code/no-more-waste/api/db.js
+++ Code/no-more-waste/api/db.js
@@ -1,1 +1,8 @@
 import mysql from "mysql"
+
+export const db = mysql.createConnection({
+    host: "db-mysql-nyc1-11769-do-user-12717269-0.b.db.ondigitalocean.com",
+    user:"doadmin",
+    passowrd:"AVNS_bZXYxAzcGMWfIdVLnWr",
+    database:"defaultdb"
+})
--- Code/no-more-waste/api/index.js
+++ Code/no-more-waste/api/index.js
@@ -4,6 +4,9 @@ const app = express()
 
 app.use(express.json())
 
+app.get("/test", (req,res)=>{
+    res.json("it works!")
+})
 
 app.listen(25060, ()=>{
     console.log("connected")
