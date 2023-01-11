0b00432c18110a51fcbde393340655f3ed86b189
Faizah Kolapo <90667389+FaizahKolapo@users.noreply.github.com>
2023-01-11 15:27:06 -0600

    Update to home page responsiveness

--- Code/no-more-waste/src/pages/Home.jsx
+++ Code/no-more-waste/src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import '../styles.css';
 import search_icon from "../images/search-icon.svg";
 import line from "../images/line.png";
 import filter from "../images/filter.png";
+import logo from "../images/noMoreWasteLogo.svg";
+import fb from "../images/fb.png";
+import instagram from "../images/instagram.png";
+import twitter from "../images/twitter.png";
+import copyright from "../images/copyright.png";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/authContext.js";
@@ -30,6 +36,21 @@ const Home = () => {
 
   return (
     <div>
+      <header>
+        <nav class="nav collapsible">
+          <a class="nav__brand" href="homePageBusiness.html"
+            ><img class="logo-class" src={logo} alt=""
+          /></a>
+          <svg class="icon icon--white nav__toggler">
+            <use href="images/sprite.svg#menu"></use>
+          </svg>
+          <ul class="list nav__list collapsible__content">
+            <li class="nav__item"><a href="#">Profile</a></li>
+            <li class="nav__item"><a href="#">Track Requests</a></li>
+            <li class="nav__item"><a href="#">Support</a></li>
+          </ul>
+        </nav>
+      </header>
       <div class="search-and-filter">
         <div class="search">
           <div class="search-div">
@@ -66,6 +87,43 @@ const Home = () => {
             <button class="card-button">REQUEST</button>
           </div>
         ))}
+      </div>
+      <div class="footer">
+        <div class="grid-footer grid--1x3">
+          <div class="footer-col-one">
+            <h3>HELP & SUPPORT</h3>
+            <p>About NOMOREWASTE</p>
+            <p>Responsibility</p>
+          </div>
+          <div class="footer-col-one">
+            <h3>SUPPORT</h3>
+            <p>Contact us</p>
+            <p>FAQs</p>
+          </div>
+          <div class="footer-col-one">
+            <h3>FIND US ON</h3>
+            <span class="footer-span"
+              ><img class="footer-icon" src={fb} alt="" />
+              <img class="footer-icon" src={instagram} alt="" />
+              <img class="footer-icon" src={twitter} alt=""
+            /></span>
+            <h3>SIGN UP FOR NEWSLETTERS</h3>
+            <span class="newsletter-span">
+              <input
+                type="text"
+                class="newsletter-input"
+                placeholder="Email address"
+              />
+              <button class="newsletter-button">SUBSCRIBE</button>
+            </span>
+          </div>
+        </div>
+        <div>
+          <span>
+            <img class="copyright-icon" src={copyright} alt="" />
+            <p class="copyright-text">2022 NOMOREWASTE All Rights Reserved</p>
+          </span>
+        </div>
       </div>
     </div>
   );
