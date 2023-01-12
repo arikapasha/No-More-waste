5fc924bef49f0048860c14d51eab4f668e3f5cee
Faizah Kolapo <90667389+FaizahKolapo@users.noreply.github.com>
2023-01-11 21:38:20 -0600

    Update footer component

--- Code/no-more-waste/src/components/Footer.jsx
+++ Code/no-more-waste/src/components/Footer.jsx
@@ -9,44 +9,43 @@ import ig from "../images/instagram.png"
 const footer = () => {
   return (
 
-    <footer class="footer">
-      
-      <div class="grid grid--1x3">
-        <div class="footer-col-one">
-          <h3>HELP & SUPPORT</h3>
-          <p>About NOMOREWASTE</p>
-          <p>Responsibility</p>
+    <div class="footer">
+        <div class="grid-footer grid--1x3">
+          <div class="footer-col-one">
+            <h3>HELP & SUPPORT</h3>
+            <p>About NOMOREWASTE</p>
+            <p>Responsibility</p>
+          </div>
+          <div class="footer-col-one">
+            <h3>SUPPORT</h3>
+            <p>Contact us</p>
+            <p>FAQs</p>
+          </div>
+          <div class="footer-col-one">
+            <h3>FIND US ON</h3>
+            <span class="footer-span"
+              ><img class="footer-icon" src={fb} alt="" />
+              <img class="footer-icon" src={instagram} alt="" />
+              <img class="footer-icon" src={twitter} alt=""
+            /></span>
+            <h3>SIGN UP FOR NEWSLETTERS</h3>
+            <span class="newsletter-span">
+              <input
+                type="text"
+                class="newsletter-input"
+                placeholder="Email address"
+              />
+              <button class="newsletter-button">SUBSCRIBE</button>
+            </span>
+          </div>
         </div>
-        <div class="footer-col-one">
-          <h3>SUPPORT</h3>
-          <p>Contact us</p>
-          <p>FAQs</p>
-        </div>
-        <div class="footer-col-one">
-          <h3>FIND US ON</h3>
-          <span class="footer-span"
-            ><img class="footer-icon" src={fb} alt="" />
-            <img class="footer-icon" src={ig} alt="" />
-            <img class="footer-icon" src={tw} alt=""
-          /></span>
-          <h3>SIGN UP FOR NEWSLETTERS</h3>
-          <span class="newsletter-span">
-            <input
-              type="text"
-              class="newsletter-input"
-              placeholder="Email address"
-            />
-            <button class="newsletter-button">SUBSCRIBE</button>
+        <div>
+          <span>
+            <img class="copyright-icon" src={copyright} alt="" />
+            <p class="copyright-text">2022 NOMOREWASTE All Rights Reserved</p>
           </span>
         </div>
       </div>
-      <div>
-        <span>
-          <img class="copyright-icon" src={cr} alt="" />
-          <p class="copyright-text">2022 NOMOREWASTE All Rights Reserved</p>
-        </span>
-      </div>
-    </footer>
   )
 }
 
