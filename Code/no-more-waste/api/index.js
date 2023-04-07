fdad25ddf29862a6f2e3edc72bd50fa4844f9a04
bulbularora <bulbularora123@gmail.com>
2023-04-07 10:36:07 -0600

    images can be now written from local host. need one more variable in .env and key file

--- Code/no-more-waste/api/index.js
+++ Code/no-more-waste/api/index.js
@@ -6,15 +6,15 @@ import cookieParser from "cookie-parser";
 import Multer from "multer";
 import { db } from "./db.js";
 import path from "path";
-import { fileURLToPath } from 'url';
-import cors from "cors"
+import { fileURLToPath } from "url";
+import cors from "cors";
+import { format } from "url";
 import { Storage } from "@google-cloud/storage";
 
 //import bodyParser from "body-parser";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 //console.log(path.join(__dirname, 'index.html'));
-
 
 const app = express();
 
@@ -24,36 +24,38 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, "../build")));
 
 
-const storage = new Storage();
-app.set('view engine', 'pug');
+const storage = new Storage({
+  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+});
+app.set("view engine", "pug");
 
 const multer = Multer({
   storage: Multer.memoryStorage(),
   limits: {
-    fileSize: 10 * 1024 * 1024, // no larger than 10mb, you can change as needed.
+    fileSize: 10 * 1024 * 1024, // no larger than 10mb
   },
 });
 
 const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
 
-app.post('/api/upload', multer.single('file'), (req, res, next) => {
+app.post("/api/upload", multer.single("file"), (req, res, next) => {
   if (!req.file) {
-    res.status(400).send('No file uploaded.');
+    res.status(400).send("No file uploaded.");
     return;
   }
 
   // Create a new blob in the bucket and upload the file data.
-  const blob = bucket.file(Date.now()+req.file.originalname);
+  const blob = bucket.file(Date.now() + req.file.originalname);
   const blobStream = blob.createWriteStream();
 
-  blobStream.on('error', err => {
+  blobStream.on("error", (err) => {
     next(err);
   });
 
-  blobStream.on('finish', () => {
+  blobStream.on("finish", () => {
     // The public URL can be used to directly access the file via HTTP.
     const publicUrl = format(
       `https://storage.googleapis.com/${bucket.name}/${blob.name}`
@@ -64,13 +66,12 @@ app.post('/api/upload', multer.single('file'), (req, res, next) => {
   blobStream.end(req.file.buffer);
 });
 
-
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
-app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname, '../build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
 });
 
 app.listen(8800, () => {
