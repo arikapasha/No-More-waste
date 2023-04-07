d048351883565bfd80d2d47404df37d306e5a279
Bulbul Arora <64860549+bulbularora@users.noreply.github.com>
2023-04-06 22:14:17 -0600

    update new tables

--- Code/tables_script.sql
+++ Code/tables_script.sql
@@ -1,28 +1,34 @@
-create table post(
-post_id int not null auto_increment,
-item_name varchar(100) not null,
-description varchar(300) not null,
-photo_link varchar(300),
-pickup_time datetime not null,
-requested bool,
-accepted bool,
-shelter_id int,
-rest_id int,
-driver_id int,
-primary key (post_id),
-foreign key (shelter_id) references user(user_id),
-foreign key (rest_id) references user(user_id),
-foreign key (driver_id) references user(user_id)
+CREATE TABLE "user" (
+  "user_id" int NOT NULL AUTO_INCREMENT,
+  "businessname" varchar(100) NOT NULL,
+  "address" varchar(100) NOT NULL,
+  "username" varchar(100) NOT NULL,
+  "phone_number" varchar(10) NOT NULL,
+  "role" varchar(1) NOT NULL,
+  "email" varchar(100) NOT NULL,
+  "password" varchar(100) NOT NULL,
+  PRIMARY KEY ("user_id")
 );
 
-create table user(
-user_id int not null auto_increment,
-businessname varchar(30) not null, 
-address varchar(100) not null,
-username varchar(100) not null,
-phone_number varchar(10) not null,
-email varchar(100) not null,
-password varchar(30) not null,
-role varchar(1) not null,
-primary key (user_id)
+
+CREATE TABLE "post" (
+  "post_id" int NOT NULL AUTO_INCREMENT,
+  "item_name" varchar(100) DEFAULT NULL,
+  "description" varchar(300) NOT NULL,
+  "photo_link" varchar(300) DEFAULT NULL,
+  "pickup_time" varchar(300) NOT NULL,
+  "requested" tinyint(1) DEFAULT NULL,
+  "accepted" tinyint(1) DEFAULT NULL,
+  "shelter_id" int DEFAULT NULL,
+  "rest_id" int DEFAULT NULL,
+  "driver_id" int DEFAULT NULL,
+  "completed" tinyint(1) DEFAULT NULL,
+  "pickedUp" tinyint(1) DEFAULT NULL,
+  PRIMARY KEY ("post_id"),
+  KEY "shelter_id" ("shelter_id"),
+  KEY "rest_id" ("rest_id"),
+  KEY "driver_id" ("driver_id"),
+  CONSTRAINT "post_ibfk_1" FOREIGN KEY ("shelter_id") REFERENCES "user" ("user_id"),
+  CONSTRAINT "post_ibfk_2" FOREIGN KEY ("rest_id") REFERENCES "user" ("user_id"),
+  CONSTRAINT "post_ibfk_3" FOREIGN KEY ("driver_id") REFERENCES "user" ("user_id")
 );
